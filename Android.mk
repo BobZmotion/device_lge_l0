5f6484637fa8775001471b06d9b09192d843c680
BobZhome <BobZhome@cheerful.com>
2013-05-07 21:20:35 -0400

    hack to get past a stop

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
 # HACK for prebuilt libril and libcamera
 $(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
 $(shell touch $(OUT)/obj/KERNEL_OBJ/usr/export_includes)
+$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libgenlock_intermediates)
+$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libgenlock_intermediates/export_includes)
